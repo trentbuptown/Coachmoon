b9ead4dfd22118cc08fac20ce4cd9475b7d5b854
DESKTOP-2TH0TA6\Bao Tran <trangiabao256@gmail.com>
2025-04-11 19:36:07 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        ringWidth: {
+          'DEFAULT': '2px',
+          '6': '6px',
+          '10': '10px',
+        ringWidth: ['hover', 'active'],
+        }
+      }
+    }
+  }
